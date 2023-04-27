3a70afe96ce750970e5a69a3efb66fe405fd44be
Tiago de Paula <tiagodepalves@gmail.com>
2023-04-27 01:26:13 -0300

    zip: Use modern C syntax
    - Declare variables when needed instead of at the top of the function
    - Use 'EXIT_FAILURE' and 'EXIT_SUCCESS' macro

--- 03-OmpTask-Zip/src/zip-parallel.c
+++ 03-OmpTask-Zip/src/zip-parallel.c
@@ -6,45 +6,39 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-const int max_password_value = 500000;
-const char cmd_format[300] = "unzip -P%d -t %s 2>&1";
-
-FILE *popen(const char *command, const char *type);
-
-int main(int argc, char **argv) {
-  FILE *fp;
-  FILE *input;
-  char filename[100];
-  char ret[200];
-  char cmd[400];
-  double t;
-  int nt;
+int main(const int argc, const char *const restrict argv[argc]) {
+  const int max_password_value = 500000;
 
   if (argc < 2) {
     fprintf(stderr, "Error: missing path to input file\n");
-    return 1;
+    return EXIT_FAILURE;
   }
 
-  if ((input = fopen(argv[1], "r")) == NULL) {
+  FILE *input = fopen(argv[1], "r");
+  if (input == NULL) {
     fprintf(stderr, "Error: could not open file\n");
-    return 1;
+    return EXIT_FAILURE;
   }
 
   // Read inputs
+  int nt;
   fscanf(input, "%d", &nt);
+  char filename[100];
   fscanf(input, "%s", filename);
 
   // Do not touch this line
   omp_set_num_threads(nt);
 
-  t = omp_get_wtime();
+  double t = omp_get_wtime();
 
   // Parallelize this loop using tasks!
   for (int i = 0; i < max_password_value; i++) {
-    sprintf((char *)&cmd, cmd_format, i, filename);
+    char cmd[400];
+    sprintf((char *)&cmd, "unzip -P%d -t %s 2>&1", i, filename);
 
-    fp = popen(cmd, "r");
+    FILE *fp = popen(cmd, "r");
     while (!feof(fp)) {
+      char ret[200];
       fgets((char *)&ret, 200, fp);
       if (strcasestr(ret, "ok") != NULL) {
         printf("Password: %d\n", i);
@@ -56,4 +50,6 @@ int main(int argc, char **argv) {
 
   t = omp_get_wtime() - t;
   fprintf(stderr, "%lf\n", t);
+
+  return EXIT_SUCCESS;
 }
